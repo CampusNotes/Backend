5253e08416b9e64686acf6d7ed2bf0569865f198
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-02-11 21:42:26 +0530

    added configuration modification

---
+++ Config/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const PORT = process.env.APP_PORT || 8000;
+
+module.exports = {
+  PORT
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const cors = require('cors')
+
+const { PORT } = require('./Config')
+
+const app = express();
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+  res.json({ msg: "hi from server" })
+})
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'Something went wrong';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage
+  });
+})
+
+app.listen(PORT, () => {
+  console.log(`server running at port ${8000}`);
+})
