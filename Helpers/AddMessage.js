6ce2fdde5deef36d2f05daff407b85a6d3796b23
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-04-14 02:08:14 +0530

    Add message helper modified

--- Helpers/AddMessage.js
+++ Helpers/AddMessage.js
@@ -4,14 +4,14 @@ async function addmessage(messageData) {
     const new_message = new Message({
         messagedata: messageData.text,
         user_id: messageData.user_id
-
     })
 
     await new_message.save()
 
-    // const allmessage = await Message.find({}).populate("user_id");
+    const data = await Message.findOne({ _id: new_message._id }).populate("user_id");
+
     // // console.log(allmessage);
-    return messageData;
+    return data;
 }
 
 module.exports = {
